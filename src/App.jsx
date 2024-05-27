d7aa5991f0c290c78227e34a9da2f348319ff82b
César Filipe António <cesarfilipeantonio2404@gmail.com>
2024-05-27 20:28:07 +0100

    create componente Limenu q tratará de todos links do menu

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ import { FaXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa6";
 import { FaYoutube } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa6";
+import { LiMenu } from "./componentes/li-menu";
 
 function App() {
 
@@ -33,39 +34,40 @@ function App() {
             <img className="w-28 h-full" src={logoTrivago} alt="Logo trivago" title="Logo trivago" />
           </h1>
           <ul className="flex">
-            <li
+            <LiMenu>
+              <RxCounterClockwiseClock className="size-5" />
+              Visualizações recentes
+            </LiMenu>
+            {/* <li
               className="flex flex-col justify-center  cursor-pointer
               text-sm text-[#0A1121] py-5 px-5 hover:bg-[#F3F4F5] 
               ">
               <di className="flex items-center justify-start gap-1.5">
-                <RxCounterClockwiseClock className="size-5" />
-                Visualizações recentes
+
               </di>
-              <div className="flex-none absolute mt-80 w-72 h-60 bg-[#FFF] rounded-md shadow-2xl 
+
+            </li> */}
+            {/* <div className="flex-none absolute mt-80 w-72 h-60 bg-[#FFF] rounded-md shadow-2xl 
               invisible hover:visible">
                 <span className="flex-none">card</span>
-              </div>
-            </li>
-            <li
-              className="flex items-center gap-1.5 text-sm text-[#0A1121] py-5 px-5 hover:bg-[#F3F4F5] cursor-pointer">
+              </div> */}
+            <LiMenu>
               <CiHeart className="size-7" />
               Favoritos
-            </li>
-            <li
-              className="flex items-center gap-1.5 text-sm text-[#0A1121] py-5 px-5 hover:bg-[#F3F4F5] cursor-pointer">
+            </LiMenu>
+            <LiMenu>
               <TfiWorld className="size-5" />
               PT.
               <LuEuro />
-            </li>
-            <li
-              className="flex items-center gap-1.5 text-sm text-[#0A1121] py-5 px-5 hover:bg-[#F3F4F5] cursor-pointer">
+            </LiMenu>
+            <LiMenu>
               <HiOutlineUserCircle className="size-6" />
               Fazer Login
-            </li>
-            <li className="flex items-center gap-1.5 text-sm text-[#0A1121] py-5 px-5 hover:bg-[#F3F4F5] cursor-pointer">
+            </LiMenu>
+            <LiMenu>
               <RiMenuLine className="size-5" />
               Menu
-            </li>
+            </LiMenu>
           </ul>
         </div>
         <hr />
